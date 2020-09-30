630c6d34236c9dc0c96c14615bc5102ce75d6f50
KC Thomas <thisiskcthomas@gmail.com>
2020-09-30 16:18:38 +0100

    Added test in DockingStation spec for creating new bike and wrote passing code.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,14 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
     it {is_expected.to respond_to(:release_bike)}
+    
+    it "creates a new bike" do
+        station = DockingStation.new 
+        bike = station.release_bike
+        expect(bike).to be_instance_of(Bike)
+    end
+
 end
