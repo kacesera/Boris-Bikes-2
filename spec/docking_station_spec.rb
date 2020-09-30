eb4bcce01cfc85561a22452b510d7c9f46ddeff6
KC Thomas <thisiskcthomas@gmail.com>
2020-09-30 15:06:22 +0100

    Added test to check if 'release_bike' method exists

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 
+    it "responds to request to release bike" do
+        station = DockingStation.new
+        expect(station.release_bike).to exist
+    end
 end
