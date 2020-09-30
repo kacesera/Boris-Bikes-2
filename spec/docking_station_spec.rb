d00bcabbc023055c95b80275f8e1c61a328e1347
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-30 16:48:41 +0100

    Set up a test to check that the docking station is giving us a working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,12 +3,11 @@ require 'bike'
 
 describe DockingStation do
 
-    it {is_expected.to respond_to(:release_bike)}
-    
-    it "creates a new bike" do
-        station = DockingStation.new 
-        bike = station.release_bike
-        expect(bike).to be_instance_of(Bike)
-    end
+  it {is_expected.to respond_to(:release_bike)}
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
 end
