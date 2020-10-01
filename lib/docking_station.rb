64aae827fbad8c9a360df85c0b030daf71a1b6dd
KC Thomas <thisiskcthomas@gmail.com>
2020-10-01 14:29:21 +0100

    Added attr_reader for instantiated bike_rack variable, to later add bike to bike_rack in dock() method.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,12 +2,19 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike_rack
+  
+  def initialize
+    @bike_rack = []
+  end
+
   def release_bike
     Bike.new
   end
 
   def dock(bike)
+    bike_rack << bike
     "Bike is docked"
   end
-  
+
 end
